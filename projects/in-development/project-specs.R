227fc27abb6be6300583052d6a157642c62e9427
aliburchard <ali@zooniverse.org>
2017-08-17 16:18:55 +0100

    cleaning datasets

--- projects/in-development/project-specs.R
+++ projects/in-development/project-specs.R
@@ -5,19 +5,31 @@
 ## If you have single or multiple choice questions and do not specify them, the flattener will ignore them. 
 ## If you do not specify column out values, the flattener will use column in values.
 # 
-# # REQUIRED FIELDS 
+# REQUIRED FIELDS
+
+# # Michigan
 # project_name <- "michigan-zoomin"
-# classifications_file <- "michigan-zoomin-filtered.csv"
+# classifications_file <- "data/michigan-zoomin-classifications.csv"
 # survey_id <- c("T3")
 # workflow_id_num <- 2276
 # workflow_version_num <- 463.55
-#     
-# # Optional Fields
 # single_choice_Qs <-  c("HOWMANYANIMALSDOYOUSEE")
 # single_choice_colnames  <-  c("how_many")
 # multi_choice_Qs <- c("WHATISTHEANIMALSDOING")
 # multi_choice_colnames <- c("behavior")
-#      
+# 
+
+# Michigan - experiment
+project_name <- "michigan-zoomin"
+classifications_file <- "data/michigan-zoomin-classifications.csv"
+survey_id <- c("T3")
+workflow_id_num <- 2276
+workflow_version_num <- 463.55
+
+# Optional Fields
+multi_choice_Qs <- c("HOWMANYANIMALSDOYOUSEE", "WHATISTHEANIMALSDOING")
+multi_choice_colnames <- c("how_many","behavior")
+
 
 # # Colorado Corridors
 # project_name <- "colorado-corridors"
@@ -47,16 +59,16 @@
 # single_choice_colnames  <-  c("how_many")
 
 
-# Chicago
-project_name <- "chicago"
-classifications_file <- "data/chicago-wildlife-watch-classifications.csv"
-workflow_id_num <- 4377
-workflow_version_num <- 2.20
-
-survey_id <- c("T0")
-single_choice_Qs <-  c("HOWMANY")
-single_choice_colnames  <-  c("how_many")
-question_task_id <- c("T1")
+# # Chicago
+# project_name <- "chicago"
+# classifications_file <- "data/chicago-wildlife-watch-classifications.csv"
+# workflow_id_num <- 4377
+# workflow_version_num <- 2.20
+# 
+# survey_id <- c("T0")
+# single_choice_Qs <-  c("HOWMANY")
+# single_choice_colnames  <-  c("how_many")
+# question_task_id <- c("T1")
 
 # Chicago also needs to deal wth these versions too, which may or may not have additional questions
 # workflow_id_num <- 2334
